3130d12c1c5143b82ede053ed0ec1afffb986cbf
Anphunghaha <anpk2300@gmail.com>
2025-07-02 08:53:30 +0700

    Update API

---
+++ BackEnd_API/API_BookStoreApp/DTOs/DTO/UserRegisterDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.DTO
+{
+    public class UserRegisterDTO
+    {
+        public string FullName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+
+        public string Username { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp")]
+        public string ConfirmPassword { get; set; }
+    }
+}
